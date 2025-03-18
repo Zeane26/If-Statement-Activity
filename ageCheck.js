ea4ff4fbf501a85e4f1e21acad55a9716d7b6f24
Zeane26 <zeaneasuit08@gmail.com>
2025-03-19 00:40:25 +0800

    Create ageCheck.js

---
+++ ageCheck.js
@@ -0,0 +1,9 @@
+let age = prompt("Enter your score:");
+ 
+ if (age <= 13) {
+     console.log("You are a child");
+ } else if (age <= 19) {
+     console.log("You are a teenager"); 
+ } else {
+     console.log("You are an adult");
+ }
